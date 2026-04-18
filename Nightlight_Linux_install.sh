ddfade653d1e3c7515fd7d50f0b4ca3857a2e5eb
ChesterTsai <tsaichester71@gmail.com>
2026-04-18 15:41:30 +0800

    trying to fix for steamOS
    people still reporting issues when running the script on steamOS,
    this might of help, idk

--- Nightlight_Linux_install.sh
+++ Nightlight_Linux_install.sh
@@ -50,15 +50,15 @@ checkSteamOS() {
         return 0
     fi
 
-    if [ "$(${ESCALATION_TOOL} steamos-readonly status)" = "enabled" ]; then
+    if [ "$(sudo steamos-readonly status)" = "enabled" ]; then
         printf "%b\n" "${YELLOW}Disabling readonly mode${RC}"
-        "$ESCALATION_TOOL" steamos-readonly disable
+        sudo steamos-readonly disable
     fi
 
     printf "%b\n" "${YELLOW}Setting up PGP keys${RC}"
-    "$ESCALATION_TOOL" pacman-key --init
-    "$ESCALATION_TOOL" pacman-key --populate archlinux
-    "$ESCALATION_TOOL" pacman-key --populate holo
+    sudo pacman-key --init
+    sudo pacman-key --populate archlinux
+    sudo pacman-key --populate holo
 
 }
 
